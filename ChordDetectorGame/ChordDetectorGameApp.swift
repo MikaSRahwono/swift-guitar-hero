c5a83272222cb013d9bc0c1c9904d4ed1523a3db
Mika Suryofakhri Rahwono <mikasuryof@outlook.com>
2024-05-16 14:39:16 +0700

    Initial Commit

---
+++ ChordDetectorGame/ChordDetectorGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChordDetectorGameApp.swift
+//  ChordDetectorGame
+//
+//  Created by Mika S Rahwono on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ChordDetectorGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChordDetectorGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChordDetectorGame
+//
+//  Created by Mika S Rahwono on 16/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
